140487ac4be9d7ec0168514dc8f2eb1b34dfbc59
Aswathi-Govindasamy <142901753+Aswathi-Govindasamy@users.noreply.github.com>
2026-01-11 20:59:04 +0530

    Update Register.jsx

--- vehicle-rental-frontend/src/pages/auth/Register.jsx
+++ vehicle-rental-frontend/src/pages/auth/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { registerUser } from "../../api/auth.api";
-import useAuth from "../../auth/UseAuth";
+import useAuth from "../../auth/useAuth";
 
 const Register = () => {
   const [name, setName] = useState("");
