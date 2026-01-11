b324b6551f0ffbc97c2236a5696d69afffd38fe9
Aswathi-Govindasamy <142901753+Aswathi-Govindasamy@users.noreply.github.com>
2026-01-11 21:32:23 +0530

    Update axios.js

--- vehicle-rental-frontend/src/api/axios.js
+++ vehicle-rental-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // IMPORTANT for auth
 });
 // Attach token automatically
