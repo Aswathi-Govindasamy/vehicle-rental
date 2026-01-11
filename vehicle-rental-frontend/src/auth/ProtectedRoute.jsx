0461b35a15046e9b93711f69695725d842b8552d
Aswathi <aswathi@Aswathis-MacBook-Air.local>
2026-01-11 20:37:35 +0530

    Standardize useAuth filename and imports

--- vehicle-rental-frontend/src/auth/ProtectedRoute.jsx
+++ vehicle-rental-frontend/src/auth/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "./UseAuth"
+import useAuth from "./useAuth";
 import Loader from "../components/common/Loader";
 
 const ProtectedRoute = () => {
